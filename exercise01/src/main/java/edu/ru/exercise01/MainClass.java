abd9de65449aa05deae4251cf19f5d5873ac6091
mirzabaig16 <tayyabmughal05@gmail.com>
2021-02-23 15:49:28 -0600

    completed exercise01

---
+++ exercise01/src/main/java/edu/ru/exercise01/MainClass.java
@@ -0,0 +1,43 @@
+
+package edu.ru.exercise01;
+
+
+public class MainClass {
+    
+
+      public static void main(String[] args) 
+      {
+          String myPlainText = "attack at noon";
+          String myEncryptedText = encrypt(myPlainText, 50);
+          
+          System.out.println(" Plain Text; " + myPlainText);
+          System.out.println("Encrypted Text; " + myEncryptedText);
+         
+       
+    }
+      
+      public static String encrypt(String plainText, int key){
+          
+          String alphabet ="abcdefghijklmnopqrstuvwxyz";
+          
+          
+          char currentChar;
+          int currentCharPos;
+          int encryptedCharPos;
+          char encryptedChar;
+          String encryptedText = "";
+          
+          for(int i = 0; i < plainText.length(); i++){
+              
+              currentChar = plainText.charAt(i);
+              currentCharPos = alphabet.indexOf(currentChar);
+              encryptedCharPos = (currentCharPos + key) % alphabet.length();
+              encryptedChar = alphabet.charAt(encryptedCharPos);
+              
+              encryptedText = encryptedText + encryptedChar;
+              
+        } 
+          
+          return encryptedText;
+    }
+}
